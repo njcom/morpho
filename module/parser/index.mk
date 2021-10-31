afa145daa97a7863b32cf7469104c1d5d5b9eae1
proto-h <7043148+proto-h@users.noreply.github.com>
2021-10-31 19:20:03 +0300

    Issue #2: Fixed tests in workflow

--- module/parser/index.mk
+++ module/parser/index.mk
@@ -1,6 +1,8 @@
-# Common make targets for the all parsers.
-
 ANTLR_JAR_FILE_PATH := /usr/share/java/antlr-*-complete.jar
+ifeq (, $(shell test -e $(ANTLR_JAR_FILE_PATH) && echo ok))
+ANTLR_JAR_FILE_PATH := $(curDirPath)/../antlr-complete.jar
+endif
+export ANTLR_JAR_FILE_PATH
 
 srcDirPath := $(curDirPath)/$(srcDirName)
 dstDirPath := $(curDirPath)/$(dstDirName)
@@ -66,7 +68,11 @@ init:
 	composer require antlr/antlr4-php-runtime
 
 setup:
-	$(binDirPath)/parser setup
+	test -e $(ANTLR_JAR_FILE_PATH) || curl -sSfL -o $(ANTLR_JAR_FILE_PATH) https://www.antlr.org/download/antlr-4.9.2-complete.jar;
+	if ! command -v antlr4 > /dev/null; then\
+		echo -e '#!/bin/sh\njava -jar "$(ANTLR_JAR_FILE_PATH)" "$$@"' >> /usr/local/bin/antlr4;\
+		chmod +x /usr/local/bin/antlr4;\
+	fi
 
 .PHONY: test-lexer tokens valid-tokens invalid-tokens test test-parser test-parser-gui ci-test antlr-lexer antlr-parser lexer parser clean init setup
 .SILENT:
