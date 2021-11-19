e11a9124b75c537665e78eb036bd64d43f82cfcf
jackstr <jackstr@users.noreply.github.com>
2021-11-19 03:43:14 +0300

    Issue #7: WIP: Pull meta about Linux distros

---
+++ .github/workflows/push-changes.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+git config --local user.name "$GITHUB_ACTOR"
+git config --local user.email "$GITHUB_ACTOR@users.noreply.github.com"
+#git config --local --unset-all "http.https://github.com/.extraheader" || true
+readonly anyChangesDone=$(git status --porcelain)
+if [[ -n "${anyChangesDone}" ]]; then
+    git add "$1"
+    git commit -m "$2"
+    git pull -s ours origin main || true
+    git push origin main:main
+else
+    echo Nothing to push
+fi
