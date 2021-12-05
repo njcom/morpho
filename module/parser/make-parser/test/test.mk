e2837c2af29c7022598ea56deaa84cf945643188
jackstr <jackstr@users.noreply.github.com>
2021-12-05 06:27:33 +0300

    Issue #9: Removed module/parser. Improved build system

--- module/parser/make-parser/test/test.mk
+++
@@ -1,8 +0,0 @@
-$(if $(filter __%, $(MAKECMDGOALS)), \
-	$(error targets prefixed with '__' are only for internal use))
-
-vmlinuxclean:
-	$(Q)$(CONFIG_SHELL) $(srctree)/scripts/link-vmlinux.sh clean
-	$(Q)$(if $(ARCH_POSTLINK), $(MAKE) -f $(ARCH_POSTLINK) clean)
-
-PHONY += archclean vmlinuxclean
